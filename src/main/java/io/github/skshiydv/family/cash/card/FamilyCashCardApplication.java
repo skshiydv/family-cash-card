8cc3232e0a7c4c48a439f89f15e3243cf83881d7
skshiydv <skshiydv@gmail.com>
2024-04-22 12:51:58 +0530

    initial commit

---
+++ src/main/java/io/github/skshiydv/family/cash/card/FamilyCashCardApplication.java
@@ -0,0 +1,13 @@
+package io.github.skshiydv.family.cash.card;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyCashCardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamilyCashCardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/skshiydv/family/cash/card/FamilyCashCardApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.skshiydv.family.cash.card;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyCashCardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
